f380122256ea0139b98ffab54be8a869ea670d99
abhinav <abhinavpadige06@gmail.com>
2026-09-05 22:26:57 +0530

    Add vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom']
-        }
-      }
-    }
+    sourcemap: true
   }
 })
